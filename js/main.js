b20688d3d221f34378b967620094bea7e9e700eb
Tara Whiteley <tarawhiteley@gmail.com>
2016-12-04 12:16:08 -0500

    fix spacing/tab and indentation issues

--- js/main.js
+++ js/main.js
@@ -1,64 +1,67 @@
 $(document).ready(function () {
+
+// Custom jQuery -- Toggle button for menu section - more, less
+// First two lines create the buttons
     $('<button class="toggle1">More</button>').appendTo('h4');
     $('<button class="toggle2">Less</button>').appendTo('h4');
-
+// Description initially hidden and will toggle on/off with the more/less button
     $('.description').hide();
-
+// Upon clicking more, this slides the description and price down to show
     $('button.toggle1').on('click', function () {
         $(this).closest('article').find('.description').slideDown('slow', function () {
             $(this).closest('article').addClass('italic');
         });
     });
-
+// Upon clicking less, this slides the description and price up to hide
     $('button.toggle2').on('click', function () {
         $(this).closest('article').find('.description').slideUp('slow', function () {
             $(this).closest('article').removeClass('italic');
         });
     });
 
+// Responsive Tabs JS - menu section allows only one section to be shown at a time
+// Starts in collapsed position on scmaller screens
     $('#responsiveTabsDemo').responsiveTabs({
         startCollapsed: 'accordion'
     });
 
-// <!-- Responsive Tabs JS -->
-    $(document).ready(function () {
-        var $tabs = $('#horizontalTab');
-        $tabs.responsiveTabs({
-            rotate: false,
-            startCollapsed: 'accordion',
-            collapsible: 'accordion',
-            setHash: true,
-            click: function (e, tab) {
-                $('.info').html('Tab <strong>' + tab.id + '</strong> clicked!');
-            },
-            activate: function (e, tab) {
-                $('.info').html('Tab <strong>' + tab.id + '</strong> activated!');
-            },
-            activateState: function (e, state) {
-                //console.log(state);
-                $('.info').html('Switched from <strong>' + state.oldState + '</strong> state to <strong>' + state.newState + '</strong> state!');
-            }
-        });
-        $('#start-rotation').on('click', function () {
-            $tabs.responsiveTabs('startRotation', 1000);
-        });
-        $('#stop-rotation').on('click', function () {
-            $tabs.responsiveTabs('stopRotation');
-        });
-        $('#start-rotation').on('click', function () {
-            $tabs.responsiveTabs('active');
-        });
-        $('#enable-tab').on('click', function () {
-            $tabs.responsiveTabs('enable', 3);
-        });
-        $('#disable-tab').on('click', function () {
-            $tabs.responsiveTabs('disable', 3);
-        });
-        $('.select-tab').on('click', function () {
-            $tabs.responsiveTabs('activate', $(this).val());
-        });
+    var $tabs = $('#horizontalTab');
+    $tabs.responsiveTabs({
+        rotate: false,
+        startCollapsed: 'accordion',
+        collapsible: 'accordion',
+        setHash: true,
+        click: function (e, tab) {
+            $('.info').html('Tab <strong>' + tab.id + '</strong> clicked!');
+        },
+        activate: function (e, tab) {
+            $('.info').html('Tab <strong>' + tab.id + '</strong> activated!');
+        },
+        activateState: function (e, state) {
+            //console.log(state);
+            $('.info').html('Switched from <strong>' + state.oldState + '</strong> state to <strong>' + state.newState + '</strong> state!');
+        }
+    });
+    $('#start-rotation').on('click', function () {
+        $tabs.responsiveTabs('startRotation', 1000);
+    });
+    $('#stop-rotation').on('click', function () {
+        $tabs.responsiveTabs('stopRotation');
+    });
+    $('#start-rotation').on('click', function () {
+        $tabs.responsiveTabs('active');
+    });
+    $('#enable-tab').on('click', function () {
+        $tabs.responsiveTabs('enable', 3);
+    });
+    $('#disable-tab').on('click', function () {
+        $tabs.responsiveTabs('disable', 3);
+    });
+    $('.select-tab').on('click', function () {
+        $tabs.responsiveTabs('activate', $(this).val());
     });
 
+//Responsive hamburger  Nav Bar
     document.getElementById('hamburgler').addEventListener('click', checkNav);
     window.addEventListener('keyup', function (e) {
         if (e.keyCode === 27) closeNav();
@@ -88,17 +91,18 @@ $(document).ready(function () {
             cl = large.classList,
             lgwrap = document.getElementById('lg-wrap');
         lgwrap.style.backgroundImage = 'url(' + large.src + ')';
-      if (cl) cl.add('hideme');
+        if (cl) cl.add('hideme');
         window.setTimeout(function () {
             large.src = new_src;
-        if(cl) cl.remove('hideme');
+            if(cl) cl.remove('hideme');
         }, 50);
         e.preventDefault();
     }, false);
 
-//Popup RSVP form
+// Custom JavaScript - Popup RSVP form
+// Reservation form tarts hidden
     $('#rsvpform').hide();
-
+// Upon clicking the button, form pops up for completion
     var reserve = document.getElementById('rsvpButton');
     reserve.addEventListener('click', function () {
         var myElement = document.getElementById('rsvpform');
